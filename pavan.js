c619903df4d9446eb276fdf5f8dc1bfbb54a362f
pavan kumar <pavankumarballala@gmail.com>
2025-09-29 19:56:22 +0530

    pavan.js

---
+++ pavan.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBars = document.querySelectorAll(".progress");
+    
+    progressBars.forEach(bar => {
+        const percentage = bar.getAttribute("data-percentage");
+        bar.style.width = percentage + '%';
+    });
+});
+document.querySelector('a[href="https://drive.google.com/uc?export=download&id=1qxld9j6E4wT1Y-THAC8b6rK_3csa985W"]').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default link behavior (if needed)
+    window.open('https://drive.google.com/uc?export=download&id=1qxld9j6E4wT1Y-THAC8b6rK_3csa985W', '_blank');
+});
+
